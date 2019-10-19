879f6500db34d1eb0534167657e027abb1377f7d
Michael Astwood <mastwood101@gmail.com>
2019-10-19 17:09:02 -0400

    fixed txt

--- Interface.py
+++ Interface.py
@@ -24,7 +24,7 @@ label.pack()
 
 #call back for Button
 def selectfile():
-    filename = askopenfilename (initialdir = "/",title = "Select file",filetypes = ("txt files","*.txt"))
+    filename = askopenfilename (initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),))
     print(filename)
 
 #button to import data in
