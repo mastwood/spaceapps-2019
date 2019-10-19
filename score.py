e6c5d93bfcee0b04cc42af33e7cef48d5f5f1a99
dreneuw <56747050+dreneuw@users.noreply.github.com>
2019-10-19 14:40:02 -0400

    score - early2

--- score.py
+++ score.py
@@ -5,13 +5,12 @@ r = 6371000
 def dist(longa,lata,longb,latb):
     print(longa,lata,longb,latb)
     dlong = longb-longa
-    dlat = latb-lata
     a = (np.cos(latb)*np.sin(dlong))**2
-    b = (np.cos(lata)*np.sin(latb)-np.sin(lata)*np.cos(latb)*np.cos(dlong))**2
+    b = ((np.cos(lata)*np.sin(latb))-(np.sin(lata)*np.cos(latb)*np.cos(dlong)))**2
     c = np.sqrt(a+b)
     d = (np.sin(lata)*np.sin(latb)+np.cos(lata)*np.cos(latb)*np.cos(dlong))
-    angle = np.arccos(c/d)
-    print(np.degrees(angle))
+    angle = np.arctan(c/d)
+    print(np.degrees(angle),angle)
     return (r*angle)
 
 print(dist(np.radians(60),np.radians(-70),np.radians(70),np.radians(-70)))
