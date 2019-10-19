fa5e62f0e2c596037085f41bde9e1b3a99124faf
dreneuw <56747050+dreneuw@users.noreply.github.com>
2019-10-18 23:39:45 -0400

    Daytime

--- prototype_1.py
+++ prototype_1.py
@@ -10,12 +10,18 @@ with open('./Data/dataset1.txt','r') as fil:
 splitdata=[]
 for i in range(1,len(da)):
     splitdata.append(da[i].split('\t\t\t\t'))
-print(splitdata)
+
 
 for i in range(1,len(splitdata)):
-    splitdata[i][len(splitdata[i])-1]=splitdata[i][len(splitdata[i])-1].split('\t\t')[0]
+    splitdata[i][len(splitdata[i])-1] = splitdata[i][len(splitdata[i])-1].split('\t\t')[0]
+
+for i in range(1, len(splitdata)):
+    if not (splitdata[i][0].split('-')[0] == '\n' or splitdata[i][0].split('-')[0] == ''):
+        splitdata[i][0] = (float(splitdata[i][0].split('-')[0]) - 1) * 24 + float(splitdata[i][0].split('-')[1])
 
 with open('cleanedData.csv','w') as fil:
     csvwriter = cs.writer(fil)
     csvwriter.writerows(splitdata)
+
+
 
